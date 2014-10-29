c4069e3265286b017036dc038faa528e4489a769
Andre Baltieri <andrebaltieri@hotmail.com>
2014-10-29 15:53:28 -0200

    Adicionado controle de usuário

---
+++ src/NgTodoList/NgTodoList.Api/app/modules/account/controllers/SignUpCtrl.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('SignUpCtrl', SignUpCtrl);
+
+    SignUpCtrl.$inject = ['$scope', 'UserRepository', '$location'];
+
+    function SignUpCtrl($scope, UserRepository, $location) {
+        $scope.signupModel = {
+            name: 'Bruce Wayne',
+            email: 'andrebaltieri@gmail.com',
+            username: 'batman',
+            password: 'batman',
+            confirmPassword: 'batman'
+        };
+
+        $scope.signup = function ($event) {
+            console.log($event);
+            UserRepository.register($scope.signupModel).then(
+                function (result) {
+                    toastr.success(result, 'Cadastro efetuado com sucesso');
+                    $location.path('/signup-success');
+                },
+                function (error) {
+                    toastr.error(error.data, 'Falha no registro');
+                });
+        }
+
+        $scope.closeModal = function (result) {
+            dialog.close(result);
+        };
+
+        $scope.matchPassword = function () {
+            return $scope.password === $scope.repassword;
+        }
+    }
+})();
---
+++ src/NgTodoList/NgTodoList.Api/app/modules/home/controllers/HomeCtrl.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeCtrl', HomeCtrl);
+
+    HomeCtrl.$inject = ['$scope','$http'];
+
+    function HomeCtrl($scope, $http) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'HomeCtrl';
+
+        activate();
+
+        function activate() { }
+
+        $scope.click = function () {
+            var serviceUrl = 'http://customer-service.azurewebsites.net/api/clientes';
+            $http.get(serviceUrl)
+                .success(function (result, status, headers, config) {
+                    console.log(result);
+                });
+        }
+    }
+})();
